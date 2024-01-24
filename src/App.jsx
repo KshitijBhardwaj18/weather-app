463020865c266fc1da24d0769e0b041ffb410d25
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-01-24 12:54:28 +0530

    Completed lab5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
+
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-  
   const API_KEY = process.env.API_KEY;
-  const url  = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}`
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
 
   return (
     <div className="w-full h-full relative">
@@ -12,6 +14,8 @@ function App() {
           type="text"
           className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md"
           placeholder="Enter location"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
         />
       </div>
     </div>
