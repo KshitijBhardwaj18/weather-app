e811639c0e138901f7c45d865cc54366b24fd85c
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-01-24 11:27:27 +0530

    Completed lab3 with input tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import {Axios} from 'axios'
+
 function App() {
 	
 
 	return (
-		<div className="text-red-600">
-			Weather App
+		<div className="w-full h-full relative">
+			<div className='text-center p-4'>
+				<input type="text" className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md" placeholder='Enter location'/>
+			</div>
 		</div>
 
 	);
